450845a5fc79010d0f222886966771b01355f818
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:23:51 +0300

    Add project files.

---
+++ TimeSpan/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeSpan
+{
+    public partial class TimeSpan : Form
+    {
+        public TimeSpan()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime time1 = DateTime.Parse(textBox1.Text);
+            DateTime time2 = DateTime.Parse(textBox2.Text);
+
+            label3.Text = (time2 - time1).ToString();
+        }
+    }
+}
